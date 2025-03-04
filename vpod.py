5ccbdb14c06d6702ba4fd97e2fb67777a5892e35
Andrei Zhabinski <andrei.zhabinski@gmail.com>
2025-03-04 10:42:48 +0100

    Auto sync down the workspace on stop()

--- vpod.py
+++ vpod.py
@@ -1,5 +1,6 @@
 #!python3
 import subprocess
+import argparse
 import json
 import os
 import random
@@ -10,6 +11,8 @@ from vastai_sdk import VastAI
 SSH_TARGET_NAME = "vast"
 WS_LOCAL_PREFIX = os.path.expanduser("~/work/")
 WS_REMOTE_PREFIX = "/home/devpod/work/"
+
+STATE_FILE = os.path.expanduser("~/.config/vpod_state.json")
 
 
 SSH_CONFIG_TEMPLATE = """
@@ -29,16 +32,6 @@ def run(cmd: str):
     return subprocess.check_output(cmd, shell=True).decode("utf-8")
 
 
-# def find_offers(sdk: VastAI, **kwargs):
-#     query = " ".join(f"{k}={v}" for k, v in kwargs.items())
-#     return sdk.search_offers(query=query)
-
-
-# def find_best_offer(sdk: VastAI, gpu_name: str = "RTX_3080", num_gpus=1):
-#     return find_offers(sdk, gpu_name=gpu_name, num_gpus=num_gpus)[0]
-
-
-
 def offer_repr(offer: dict):
     s = f"{len(offer['gpu_ids'])} x {offer['gpu_name']}"
     s += f" (CUDA {offer['cuda_max_good']})"
@@ -53,15 +46,37 @@ def sync_workspace(workspace: str, up=True):
     if up:
         run(f"rsync -avz {local}/ {remote}")
     else:
-        run(f"rsync -avz {remote} {local}")
+        run(f"rsync -avz {remote}/ {local}")
+
+
+def update_ssh_config(ssh_host: str, ssh_port: int):
+    with open(os.path.expanduser("~/.ssh/config.d/vast"), "w") as fp:
+        config = SSH_CONFIG_TEMPLATE.format(ssh_host=ssh_host, ssh_port=ssh_port)
+        fp.write(config)
+    attempts = 3
+    success = False
+    while not success and attempts > 0:
+        try:
+            run(f"ssh-keygen -R '[{ssh_host}]:{ssh_port}' | true")  # remove old occurrences
+            run(f"ssh-keyscan -p {ssh_port} {ssh_host} >> ~/.ssh/known_hosts")     # add this host to known
+            success = True
+        except:
+            attempts -= 1
+            if attempts == 0:
+                raise
+            else:
+                print("Failed to update host keys, retrying in 3 seconds...")
+                time.sleep(3)
 
 
 def start(image: str, query: str, workspace: str | None = None):
     sdk = VastAI()
+
     # find an offer
     offers = sdk.search_offers(query=query)
     offer = offers[random.randint(0, len(offers[:5]))]
     print(f"Renting {offer_repr(offer)}")
+
     # launch instance
     resp = sdk.create_instance(id=offer["id"], image=image, onstart_cmd=ONSTART_CMD)
     if not resp["success"]:
@@ -74,21 +89,22 @@ def start(image: str, query: str, workspace: str | None = None):
         time.sleep(1)
         instance = sdk.show_instance(id=instance_id)
         status = instance["actual_status"]
+
     # set up SSH
-    ssh_host = instance["ssh_host"]
-    ssh_port = instance["ssh_port"]
-    with open(os.path.expanduser("~/.ssh/config.d/vast"), "w") as fp:
-        config = SSH_CONFIG_TEMPLATE.format(ssh_host=ssh_host, ssh_port=ssh_port)
-        fp.write(config)
-    run(f"ssh-keygen -R '[{ssh_host}]:{ssh_port}' | true")  # remove old occurrences
-    run(f"ssh-keyscan -p {ssh_port} {ssh_host} >> ~/.ssh/known_hosts")     # add this host to known
+    update_ssh_config(instance["ssh_host"], instance["ssh_port"])
     if workspace is not None:
         sync_workspace(workspace, up=True)
+
+    # save state
+    with open(STATE_FILE, "w") as fp:
+        json.dump({"instance_id": instance["id"], "workspace": workspace}, fp)
     return instance
 
 
 def stop():
     sdk = VastAI()
+    with open(STATE_FILE) as fp:
+        state = json.load(fp)
     instances = sdk.show_instances()
     if len(instances) == 0:
         raise ValueError("No instances are running")
@@ -97,9 +113,19 @@ def stop():
             f"Expected exactly 1 running instance, " +
             f"but instead found {len(instances)}"
         )
+    if instances[0]["id"] != state["instance_id"]:
+        raise ValueError(
+            f"Inconsistent state: instance_id in state file is {state['instance_id']}, " +
+            f"but running instance with id {instances[0]['id']}"
+        )
+    workspace = state["workspace"]
+    if workspace:
+        sync_workspace(workspace, up=False)
     sdk.destroy_instance(id=instances[0]["id"])
 
 
+
+###########################################################
 
 def main():
     image = "faithlessfriend/equilibrium:dev"
@@ -107,52 +133,3 @@ def main():
     workspace = "equilibrium"
     instance = start(image, query, workspace)
     stop()
-
-    # TODO: create real instance and check actual error (current one is due to non-existing ID)
-
-    sdk = VastAI()
-    resp = sdk.create_instance(id=15530478, )
-    instance = sdk.show_instance(id=resp["new_contract"])
-    print(f"{instance['ssh_host']}:{instance['ssh_port']}")
-    # s = sdk.ssh_url(id=r["new_contract"])
-
-    req_url = "https://console.vast.ai/api/v0/instances?owner=me&api_key=876f578cec5365c77718045660b78a47b738dcdbc9bb147f500ff06355d7b35a"
-
-    import sys
-    import os
-
-    sys.stdout = os.fdopen(1, 'w')
-    sys.stderr = os.fdopen(2, 'w')
-
-
-    import argparse
-    args = argparse.Namespace(api_key=sdk.api_key, id=1234, url="https://console.vast.ai/api/v0")
-
-    self = sdk
-    kwargs = {"id": 15530478}
-
-    method_name = "ssh_url"
-    arg_details = self.imported_methods.get(method_name, {})
-    for arg, details in arg_details.items():
-        if details["required"] and arg not in kwargs:
-            raise ValueError(f"Missing required argument: {arg}")
-        if (
-            arg in kwargs
-            and details.get("choices") is not None
-            and kwargs[arg] not in details["choices"]
-        ):
-            raise ValueError(
-                f"Invalid choice for {arg}: {kwargs[arg]}. Valid options are {details['choices']}"
-            )
-        kwargs.setdefault(arg, details["default"])
-
-    kwargs.setdefault("api_key", self.api_key)
-    kwargs.setdefault("url", self.server_url)
-    kwargs.setdefault("retry", self.retry)
-    kwargs.setdefault("raw", self.raw)
-    kwargs.setdefault("explain", self.explain)
-    kwargs.setdefault("quiet", self.quiet)
-
-# GPU_NAME=RTX_3080
-
-# OFFERS=$(vastai search offers 'num_gpus = 1 gpu_name = ${GPU_NAME}')
